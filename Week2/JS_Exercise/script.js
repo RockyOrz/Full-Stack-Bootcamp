93d78bf3233b5325ae4569261693dab2036a6fcf
RockyOrz <lyk8920@gmail.com>
2021-06-08 18:15:49 -0400

    Week 2 Day2

---
+++ Week2/JS_Exercise/script.js
@@ -0,0 +1,34 @@
+
+// function bigger(a, b){
+//     return a > b ? console.log(`a is bigger, ${a}`) : console.log(`b is bigger, ${b}`);
+// }
+
+// bigger(10, 8);
+
+// function evenOdd() {
+//     for (i = 1; i < 16; i++) {
+//         i%2 == 0 ? console.log(`${i} is even`) : console.log(`${i} is odd`);
+//     }
+// }
+    
+//evenOdd();
+
+// function sum5() {
+//     let sum=0;
+//     for (let i = 0; i <=100; i++) {
+//         i%5 == 0 ? sum+=i : null;
+//     }
+//     // console.log(sum);
+//     alert(sum);
+// }
+
+function sum100() {
+    let sum=0;
+    for (let i = 0; i <=100; i++) {
+        sum+=i;
+    }
+    console.log(sum);
+    // alert(sum);
+}
+
+
